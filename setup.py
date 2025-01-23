351e0afe08c622474400172aa693b24ab16385d4
k-vanshhh <vanshkansal5@gmail.com>
2025-01-24 00:29:14 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+setup(
+    name="Topsis-Vansh-102203021",
+    version="0.1.1",  # Incremented version number
+    author="Vansh",
+    author_email="your_email@example.com",
+    description="A Python package for implementing the TOPSIS decision-making method.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YourGitHubUsername/Topsis-Vansh-102203021",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "openpyxl"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
